eae215d5bae18aa257cd1650cbf0cdf449fa2a5a
GuilhermeRoque <roqueee007@gmail.com>
2023-04-23 19:54:25 -0300

    Remove sqlalchemy from migrations

--- db.py
+++ db.py
@@ -5,7 +5,10 @@ from sqlalchemy.ext.declarative import declarative_base
 
 DATABASE_URL = f"postgresql+asyncpg://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_DATABASE')}"
 
+# central source of connections to a particular database, providing both a factory and a holding space called
+# a connection pool for these database connections.
 async_engine = create_async_engine(DATABASE_URL, future=True, echo=True, pool_size=10, max_overflow=5)
+
 AsyncSessionFactory = async_sessionmaker(async_engine, expire_on_commit=False)
 
 Base = declarative_base()
